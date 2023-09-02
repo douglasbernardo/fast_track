1e6c32d373207dd7242e068519c7547113981dbd
Douglas Melo <douglas@emiolo.com>
2023-09-02 20:21:30 -0300

    usando o pinia

---
+++ app/store/authStore.ts
@@ -0,0 +1,31 @@
+
+import axios from "axios"
+import { defineStore } from "pinia"
+
+export const useStore = defineStore('user', {
+  state: () => ({
+    user: localStorage.getItem('user'),
+    token: localStorage.getItem('token')
+  }),
+
+  actions: {
+    async signIn(email: string, pass:string) {
+      await axios.post('http://localhost:3030/auth/login',{
+        email: email,
+        password: pass
+      }).then((res)=>{
+        this.user = res.data.payload.name
+        this.token = res.data.access_token
+        localStorage.setItem('user',res.data.payload.name)
+        localStorage.setItem('token',res.data.access_token)
+        navigateTo('/')
+      })
+    },
+    logout(){
+      this.user = null
+      this.token = null
+      localStorage.clear()
+      navigateTo('/')
+    }
+  }
+})
